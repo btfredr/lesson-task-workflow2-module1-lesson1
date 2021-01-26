b078c8a605bdd0a308e076b2ef7ae83bebb2b8cf
Fredrik <fredrikf2000@gmail.com>
2021-01-26 14:04:32 +0100

    finished tasks

---
+++ public/js/ts/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// Question 1
+let number;
+let string;
+let boolean;
+let info;
+number = 123;
+string = "Hello";
+boolean = true;
+info = "Idk just typing something random";
+// Question 2
+let array = [true, false, false, true];
+// Question 3
+let array2 = [12, "Hello", "Good Morning", 25];
+// Question 4
+const tuple = ["hello", "there", 123];
+// Question 5
+function params(val1, val2) {
+    val1 = "Hello";
+    val2 = 123;
+}
---
+++ src/ts/script.ts
@@ -0,0 +1,38 @@
+// Question 1
+let number: number;
+let string: string;
+let boolean: boolean;
+let info: any;
+
+number = 123;
+string = "Hello";
+boolean = true;
+info = "Idk just typing something random";
+
+// Question 2
+
+let array: boolean[] = [true, false, false, true];
+
+// Question 3
+
+let array2 = [12, "Hello", "Good Morning", 25];
+
+// Question 4
+
+const tuple = ["hello", "there", 123]
+
+
+// Question 5
+
+function params(val1: string | boolean, val2: number | string) {
+    val1 = "Hello";
+    val2 = 123;
+}
+
+
+
+
+
+
+
+
